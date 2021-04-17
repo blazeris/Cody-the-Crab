1dc4e2bbfd209a675c7a65311aef4c8e3f4d7077
blazeris <blazeris@users.noreply.github.com>
2021-04-17 12:02:32 -0700

    first commit

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Connected')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
