2a9505348b7aa0bfd10b718c4840f6f64dd9640a
Vaineee <81544089+Vaineee@users.noreply.github.com>
2021-04-17 21:32:25 -0700

    Help Function
    Updated help with a better helping code

--- cogs/test.py
+++ cogs/test.py
@@ -1,7 +1,5 @@
 from discord.ext import commands
 
-description = ("!ping will make the bot respond with PONG")
-
 class Test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -12,8 +10,33 @@ class Test(commands.Cog):
     async def ping(self, context):
         await context.send("PONG")
 
+    @commands.command(name = 'pingg', description = 'When ping, the bot will PONG')
+    async def pingg(self, context):
+        await context.send("PONGG")
 
+    @commands.command(name = 'pinggg', description = 'When ping, the bot will PONG')
+    async def pinggg(self, context):
+        await context.send("PONGGG")
 
+    @commands.command(name = 'pingggg', description = 'When ping, the bot will PONG')
+    async def pingggg(self, context):
+        await context.send("PONGGGG")
+
+    @commands.command(name = 'pinggggg', description = 'When ping, the bot will PONG')
+    async def pinggggg(self, context):
+        await context.send("PONGGGGGgggggggggggggggggggggggggggggggg")
+
+    @commands.command(name = 'pingggggg', description = 'When ping, the bot will PONG')
+    async def pingggggg(self, context):
+        await context.send("PONGGGGGgggggggggggggggg")
+
+    @commands.command(name = 'pinggggggg', description = 'When ping, the bot will PONG')
+    async def pinggggggg(self, context):
+        await context.send("PONGGGGGgggggggggggggggg")
+
+    @commands.command(name = 'pingggggggg', description = 'When ping, the bot will PONG')
+    async def pingggggggg(self, context):
+        await context.send("PONGGGGGgggggggg")
 def setup(bot):
     bot.add_cog(Test(bot))
     print("in setup")
