4f6710ac03af28e6f1850915db30c7df26270566
Vaineee <81544089+Vaineee@users.noreply.github.com>
2021-04-18 01:00:15 -0700

    para nathan
    nathan

--- cogs/test.py
+++ cogs/test.py
@@ -10,33 +10,6 @@ class Test(commands.Cog):
     async def ping(self, context):
         await context.send("PONG")
 
-    @commands.command(name = 'pingg', description = 'When ping, the bot will PONG')
-    async def pingg(self, context):
-        await context.send("PONGG")
-
-    @commands.command(name = 'pinggg', description = 'When ping, the bot will PONG')
-    async def pinggg(self, context):
-        await context.send("PONGGG")
-
-    @commands.command(name = 'pingggg', description = 'When ping, the bot will PONG')
-    async def pingggg(self, context):
-        await context.send("PONGGGG")
-
-    @commands.command(name = 'pinggggg', description = 'When ping, the bot will PONG')
-    async def pinggggg(self, context):
-        await context.send("PONGGGGGgggggggggggggggggggggggggggggggg")
-
-    @commands.command(name = 'pingggggg', description = 'When ping, the bot will PONG')
-    async def pingggggg(self, context):
-        await context.send("PONGGGGGgggggggggggggggg")
-
-    @commands.command(name = 'pinggggggg', description = 'When ping, the bot will PONG')
-    async def pinggggggg(self, context):
-        await context.send("PONGGGGGgggggggggggggggg")
-
-    @commands.command(name = 'pingggggggg', description = 'When ping, the bot will PONG')
-    async def pingggggggg(self, context):
-        await context.send("PONGGGGGgggggggg")
 def setup(bot):
     bot.add_cog(Test(bot))
     print("in setup")
